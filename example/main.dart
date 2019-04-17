a423526273d8b91e5e3fe106536e6fdda3adea20
Daniel Vilela <dmvvilela@gmail.com>
2019-04-17 12:14:43 -0300

    flutter packages get not working

---
+++ example/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:unicorndial/unicorndial.dart';
+
+void main() =>
+    runApp(new MaterialApp(debugShowCheckedModeBanner: false, home: Example()));
+
+class Example extends StatefulWidget {
+  _Example createState() => _Example();
+}
+
+class _Example extends State<Example> {
+  @override
+  Widget build(BuildContext context) {
+    var childButtons = List<UnicornButton>();
+
+    childButtons.add(UnicornButton(
+        hasLabel: true,
+        labelText: "Choo choo",
+        currentButton: FloatingActionButton(
+          heroTag: "train",
+          backgroundColor: Colors.redAccent,
+          mini: true,
+          child: Icon(Icons.train),
+          onPressed: () {},
+        )));
+
+    childButtons.add(UnicornButton(
+        currentButton: FloatingActionButton(
+            heroTag: "airplane",
+            backgroundColor: Colors.greenAccent,
+            mini: true,
+            child: Icon(Icons.airplanemode_active))));
+
+    childButtons.add(UnicornButton(
+        currentButton: FloatingActionButton(
+            heroTag: "directions",
+            backgroundColor: Colors.blueAccent,
+            mini: true,
+            child: Icon(Icons.directions_car))));
+
+    return Scaffold(
+        floatingActionButton: UnicornDialer(
+            backgroundColor: Color.fromRGBO(255, 255, 255, 0.6),
+            parentButtonBackground: Colors.redAccent,
+            orientation: UnicornOrientation.VERTICAL,
+            parentButton: Icon(Icons.add),
+            childButtons: childButtons),
+        appBar: AppBar(),
+        body: Center(child: RaisedButton(
+          onPressed: () {
+            setState(() {});
+          },
+        )));
+  }
+}
